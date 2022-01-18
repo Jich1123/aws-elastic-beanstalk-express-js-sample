73aa7e586ca57cd8ea04074951b368f23c1e6d7f
Jich <1002655916@qq.com>
2022-01-18 13:47:23 +0800

    手动执行

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! Jich v2.0'));
+app.get('/', (req, res) => res.send('Hello World! Jich v3.0 手动执行'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
